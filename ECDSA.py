86bc3fdc97ec84882fb602c8c3504facd25ed4a7
tokimay <tokimay@gmail.com>
2024-04-09 07:49:43 +0330

    params

--- ECDSA.py
+++ ECDSA.py
@@ -14,18 +14,18 @@ def sign(message: str, privateKey: str) -> tuple[int, int]:
     r = r[0] % secp256k1.n()
     if r == 0:
         return sign(message, privateKey)
-    k_inverse = pow(k, -1, secp256k1.n())
-    s = k_inverse * (h + r * p) % secp256k1.n()
+    kInv = pow(k, -1, secp256k1.n())
+    s = kInv * (h + r * p) % secp256k1.n()
     return r, s
 
 
 def verify(signature: tuple, message: str, public_key: tuple) -> bool:
     (r, s) = signature
     h = int((sha256(message.encode('utf-8')).hexdigest()), 16)
-    s_inverse = pow(s, -1, secp256k1.n())
+    sInv = pow(s, -1, secp256k1.n())
     # c = ((h * s_inverse) * G) + ((r * s_inverse) * publicKey)
-    u = h * s_inverse % secp256k1.n()
-    v = r * s_inverse % secp256k1.n()
+    u = h * sInv % secp256k1.n()
+    v = r * sInv % secp256k1.n()
     p1 = secp256k1.multipy(repeat=v, point=public_key)
     p2 = secp256k1.multipy(repeat=u, point=secp256k1.g())
     c = secp256k1.add(p1, p2)
