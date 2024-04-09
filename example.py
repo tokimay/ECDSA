71daba9b9b9b22b3304945c2fc0628ed6bd2f7bd
tokimay <tokimay@gmail.com>
2024-04-09 07:52:48 +0330

    params

--- example.py
+++ example.py
@@ -3,10 +3,10 @@ import ECDSA
 
 
 message = "Hello"
-privateKey = hex(69906862961406275191037665143949518378358884783178670810968073317421786058782)
-publicKeyCoordinate = secp256k1.getPublicKeyCoordinate(int(privateKey, 16))
+privateKey = 69906862961406275191037665143949518378358884783178670810968073317421786058782
+publicKeyCoordinate = secp256k1.getPublicKeyCoordinate(privateKey)
 
-r, s = signature = ECDSA.sign(message, privateKey)
+r, s = signature = ECDSA.sign(message, hex(privateKey))
 print(f"r = {r}")
 print(f"s = {s}")
 print(ECDSA.verify(signature, message, publicKeyCoordinate))
